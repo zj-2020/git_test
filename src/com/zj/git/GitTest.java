604b6bbd7e2685eceee203bc4f0ca2a8505d0966
zhangjing <zhangjing98012006@163.com>
2020-06-24 11:11:02 +0800

    更新文件内容

--- src/com/zj/git/GitTest.java
+++ src/com/zj/git/GitTest.java
@@ -2,6 +2,7 @@ package com.zj.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("推送到远程");
+        System.out.println("推送到本地");
+        System.out.println("更新本地仓库");
     }
 }
