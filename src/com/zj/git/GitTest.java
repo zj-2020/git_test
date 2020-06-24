65fd620205aee65aacbc4bcf74344abe92ad8740
zhangjing <zhangjing98012006@163.com>
2020-06-24 11:22:08 +0800

    添加分支

--- src/com/zj/git/GitTest.java
+++ src/com/zj/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("推送到本地");
         System.out.println("更新本地仓库");
         System.out.println("更新远程仓库");
+        System.out.println("添加分支");
     }
 }
