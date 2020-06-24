963c2bf0b0eaa128bdfecb2a4648b5e948329cac
zhangjing <zhangjing98012006@163.com>
2020-06-24 11:28:34 +0800

    合并分支

--- src/com/zj/git/GitTest.java
+++ src/com/zj/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("推送到本地");
         System.out.println("更新本地仓库");
         System.out.println("更新远程仓库");
+        System.out.println("合并分支到主干");
     }
 }
