5dd11f99cb069e5d2060649323fbd6fda0ecd91f
gitTest01 <58585295+zj-2020@users.noreply.github.com>
2020-06-24 11:13:40 +0800

    Update GitTest.java

--- src/com/zj/git/GitTest.java
+++ src/com/zj/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("推送到本地");
         System.out.println("更新本地仓库");
+        System.out.println("从远处仓库下载");
     }
 }
